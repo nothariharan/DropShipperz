938c2b3db1f094d0008811c92bb231b87433c9d9
nothariharan <nothariharan@gmail.com>
2025-09-27 14:57:58 +0530

    first commit every file set up so far

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,28 @@
+import './Nav.css'
+import { FiHeart } from "react-icons/fi";
+import { FaRegUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+
+function Nav() {
+  return (
+    <nav>
+      <div className='nav-container'>
+        <input type='text' className='search-input' placeholder='Search'></input>
+      </div>
+
+      <div className='profile-container'>
+        <a href = '#'>
+          <FiHeart className='nav-icons'/>
+        </a>
+        <a href = '#'>
+          <FaRegUser className='nav-icons'/>
+        </a>
+        <a href = '#'>
+          <FaShoppingCart className='nav-icons'/>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
