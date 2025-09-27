3758eae61eb69a3f433aa80e9a4c3c24d1e677b0
nothariharan <nothariharan@gmail.com>
2025-09-27 18:50:57 +0530

    added sidebar with the mini logo text on top left with the category filters side bar as well with style ofcofc

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Divide } from "lucide-react";
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
-
+import Sidebar from "./Sidebar/Sidebar";
 function App() {
   return (
     <>
+    <Sidebar/>
     <Nav/>
-    <Products />
     <Recommended />
+    <Products />
+    
     </>
   );
 }
