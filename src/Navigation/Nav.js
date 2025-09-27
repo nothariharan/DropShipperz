5723c86f8011e49be71a2cf7faaccf72c1eaddaa
nothariharan <nothariharan@gmail.com>
2025-09-27 17:00:51 +0530

    updated products card and give style to it didnt restrucutre it to work withe data on db yet but card with style and stuff is donzo

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -2,11 +2,14 @@ import './Nav.css'
 import { FiHeart } from "react-icons/fi";
 import { FaRegUser } from "react-icons/fa";
 import { FaShoppingCart } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+
 
 function Nav() {
   return (
     <nav>
       <div className='nav-container'>
+        <FaSearch className='nav-icons-search'/>
         <input type='text' className='search-input' placeholder='Search'></input>
       </div>
 
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,8 +1,33 @@
 import './Products.css'
+import { FaStar } from "react-icons/fa6";
+import { IoBagOutline } from "react-icons/io5";
+
 function Products() {
-  return (
-    <div>Products</div>
-  )
+  return (<>
+    <section className='card-container'>
+      <section className='card'>
+        <img src='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'alt='Nike Air Monarch 1' className='card-image'/>
+        <div className='card-details'>
+          <h3 className='card-title'>Air Monarch 1</h3>
+          <section className='card-reviews'>
+            <FaStar className='rating-star'/>  <FaStar className='rating-star' />  <FaStar className='rating-star'/>  <FaStar className='rating-star'/>  <FaStar className='rating-star'/>
+            <span className='total-reviews'>4</span>
+            </section>
+            <section className='card-price'>
+              <div className='price'>
+                <del>$690</del>   $500
+              </div>
+              <div className='bag'>
+                <IoBagOutline className='bag-icon'/>
+              </div>
+            </section>
+        </div>
+      </section>
+      
+    </section>
+  
+      
+    </>)
 }
 
 export default Products
